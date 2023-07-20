4360286305874915497d9092a16d813a8c5f10e3
Glaucia Magalhaes <g.alemagalhaes@gmail.com>
2023-07-20 15:59:33 -0300

    corrige experiencia

--- assets/js/main.js
+++ assets/js/main.js
@@ -58,10 +58,24 @@ function updatePortfolio(profileData) {
     }).join("");
 }
 
+function updateProfessionalExperience(profileData){
+  const professionalExperience = document.getElementById('profile.professionalExperience')
+  professionalExperience.innerHTML = profileData.professionalExperience.map(experience => {
+    return `
+      <li>
+        <h3 class="title">${experience.name}</h3>
+        <p class="period">${experience.period}</p>
+        <p>${experience.description}</p>
+      </li>
+    `
+  }).join("")
+}
+
 (async () => {
   const profileData = await fetchProfileData();
   updateProfileInfo(profileData);
   updateSoftSkills(profileData);
   updateHardSkills(profileData);
   updatePortfolio(profileData);
+  updateProfessionalExperience(profileData)
 })();
